0d2d575c30ed2b0653b8e8c9e01447d37440ebb7
shahidansari311 <shahidansari945256@gmail.com>
2026-08-08 13:48:00 +0530

    refactor: simplify prisma configuration and update build scripts to manually clean output directories

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -1,14 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
-const config_1 = require("prisma/config");
-exports.default = (0, config_1.defineConfig)({
-    schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-    },
-    datasource: {
-        url: process.env["DATABASE_URL"],
-    },
+const { defineConfig } = require("prisma/config");
+
+module.exports = defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
 });
-//# sourceMappingURL=prisma.config.js.map
